ccf29701b05312f43a105132cdb9acebecd70f85
albert19_ <albertnanda1902@gmail.com>
2023-12-18 20:03:41 +0800

    add logo on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className='flexBetween navbar'>
+            <div className='flex-1 flexStart gap-10'>
+                <Link href="/">
+                    <Image
+                        src="/logo.png"
+                        width={115}
+                        height={43}
+                        alt='AlTech'
+                    />
+                </Link>
+                <ul className='xl:flex hidden text-small gap-7'>
+
+                </ul>
+            </div>
+        </nav>
     )
 }
 
